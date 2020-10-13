9954655e341e9b303c03f900fc8e4dfcbbd12f59
AGMAndirko <sanyi.personal@gmail.com>
2020-10-13 09:45:44 +0200

    GO cone plots & code

--- code/ExPecto/GO/GO_catg001/GO_ExPecto_analysisplots.R
+++ code/ExPecto/GO/GO_catg001/GO_ExPecto_analysisplots.R
@@ -2,6 +2,7 @@ library(tidyverse)
 library(biomaRt)
 library(reshape2)
 library(gprofiler2)
+library(ggrepel)
 
 output0_300 <- read_csv("output0_300.csv") 
 terms_n_genes <- read_csv("terms_n_genes")
@@ -45,3 +46,81 @@ ggplot(test, aes(x = sum, , y = tissue)) +
   theme_minimal() +
   ggtitle("Abs value of expression in GO-enriched genes", subtitle = "By tissue (0-300kya)") +
   geom_col()
+
+#Now for the cone plots
+rm(list = ls())
+
+#Import
+output0300 <- read_csv("output0_300.csv")
+output300500 <- read_csv("output300_500.csv")
+output500800 <- read_csv("output500_800.csv")
+
+#select columns
+
+dataprep <- function (timewindow) {
+  output <- timewindow[,-(1:10),drop=FALSE] 
+  
+  output <- output %>% 
+    dplyr::select(3,8,9, 10, 11, 12, 13, 14, 15, 16,17,18,20, 42, 61, 136, 145, 161, 186, 187, 189, 209) %>% 
+    melt()
+  output$genes <- timewindow$gene
+  
+  inp <- aggregate(value ~ genes, output, sum)
+  colnames(inp)[2] <- "directionality"
+  
+  inpm <- aggregate(abs(value) ~ genes, output, sum)
+  colnames(inpm)[2] <- "magnitude"
+  
+  test <- inp$genes == inpm$genes
+  FALSE %in% test
+  inp$magnitude <- inpm$magnitude
+  return(inp)
+}
+
+output0300 <- dataprep(output0300)
+output300500 <- dataprep(output300500)
+output500800 <- dataprep(output500800)
+
+plotinp <- rbind(output0300, output300500, output500800)
+plotinp$timing <- c(rep("0 - 300k", nrow(output0300)),
+                    rep("300k - 500k", nrow(output300500)),
+                    rep("500k - 800k", nrow(output500800)))
+
+
+#order levels 
+plotinp$timing <- factor(plotinp$timing, levels = c("0 - 300k", "300k - 500k", "500k - 800k"))
+
+#clean a bit
+rm(output060, output60100, output100200, output200300, output300500, output500800)
+
+genenames<- gconvert(query = plotinp$genes, organism = "hsapiens",
+                     target="ENSG", filter_na = FALSE)
+
+test <- genenames$input == plotinp$genes
+# Interestingly there are targets here that produce FALSE! 
+# No clue why though
+FALSE %in% test # should be FALSE
+#produces some NA! some ensemble ID genes are not recognized for some reason (71 genes)
+#I can live with that
+plotinp$genes <- as.character(genenames$name)
+
+#plot like in the Expecto paper!
+pdf("GOconeplot.pdf")
+ggplot(plotinp, aes(x=magnitude, y=directionality, colour = timing, label = genes)) +
+  theme_minimal() +
+  ggtitle("GO genes only") +
+  geom_point(size = 3) +
+  labs(x= "Absolute magnitude of change", y = "Directionality (altternative allele)") +
+  geom_label_repel(data = subset(plotinp,  magnitude > 2), colour = "black", nudge_y = 1 ) 
+dev.off()
+
+pdf("GOwrapped.pdf")
+ggplot(plotinp, aes(x=magnitude, y=directionality, colour = timing, label = genes)) +
+  theme_minimal() +
+  ggtitle("GO genes only") +
+  geom_point(size = 3) +
+  labs(x= "Absolute magnitude of change", y = "Directionality (altternative allele)") +
+  geom_label_repel(data = subset(plotinp,  magnitude > 2), colour = "black", nudge_y = 1 ) +
+  facet_wrap(timing ~ ., ncol = 2)
+dev.off()
+
